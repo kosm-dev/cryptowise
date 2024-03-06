2415b8d36e7041a2ddd31b60f983faa0d7d6f8c8
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2024-03-06 12:28:14 +0200

    added header, footer and section top

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,52 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+
+  if (document.querySelector('.top-slider')) {
+    const topSlider = new Swiper('.top-slider', {
+      loop: true,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
